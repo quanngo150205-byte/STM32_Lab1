26d3b0219a7f2dea1df44b4e3e7a0c35aa448223
quanngo150205-byte <quan.ngo150205@hcmut.edu.vn>
2025-09-16 22:46:42 +0700

    update code and debug file Ex6

--- Lab1Ex/Core/Src/main.c
+++ Lab1Ex/Core/Src/main.c
@@ -43,16 +43,48 @@
 
 /* USER CODE BEGIN PV */
 
+	GPIO_TypeDef* LED_PORT[12] = {
+    LED_12_GPIO_Port, LED_1_GPIO_Port, LED_2_GPIO_Port, LED_3_GPIO_Port,
+    LED_4_GPIO_Port,  LED_5_GPIO_Port, LED_6_GPIO_Port, LED_7_GPIO_Port,
+    LED_8_GPIO_Port,  LED_9_GPIO_Port, LED_10_GPIO_Port, LED_11_GPIO_Port
+	};
+
+	uint16_t LED_PIN[12] = {
+	    LED_12_Pin, LED_1_Pin, LED_2_Pin, LED_3_Pin,
+	    LED_4_Pin,  LED_5_Pin, LED_6_Pin, LED_7_Pin,
+	    LED_8_Pin,  LED_9_Pin, LED_10_Pin, LED_11_Pin
+	};
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+
+// Hàm bật LED (active low)
+void setLED(int pos) {
+    HAL_GPIO_WritePin(LED_PORT[pos], LED_PIN[pos], GPIO_PIN_RESET);
+}
+
+// Hàm tắt LED (active low)
+void resetLED(int pos) {
+    HAL_GPIO_WritePin(LED_PORT[pos], LED_PIN[pos], GPIO_PIN_SET);
+}
+
+// Cập nhật kim (giữ LED cũ sáng, chỉ tắt khi nhảy sang LED mới)
+void updateHand(int pos, int *prev_pos) {
+    if (*prev_pos != pos) {
+        resetLED(*prev_pos);   // tắt LED cũ
+        setLED(pos);           // bật LED mới
+        *prev_pos = pos;
+    }
+}
 
 /* USER CODE END 0 */
 
@@ -83,15 +115,44 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
+  int hour = 0, min = 0, sec = 0;
+  int prev_sec = 0, prev_min = 0, prev_hour = 0;
 
+  // Ban đầu: bật LED_12 cho cả 3 kim
+  setLED(0);
+  prev_sec = prev_min = prev_hour = 0;
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  //ROOT
+	  // Tính vị trí LED của các kim
+	  int secPos  = sec / 5;               // 60s → 12 LED
+	  int minPos  = min / 5;               // 60p → 12 LED
+	  int hourPos = (hour % 12);           // 12h → 12 LED
+
+	  updateHand(secPos,  &prev_sec);
+	  updateHand(minPos,  &prev_min);
+	  updateHand(hourPos, &prev_hour);
+
+	  HAL_Delay(200); // 200ms thực = 1s mô phỏng
+
+	  // Tăng thời gian mô phỏng
+	  sec++;
+	  if (sec >= 60) {
+	      sec = 0;
+	      min++;
+	  }
+	  if (min >= 60) {
+	      min = 0;
+	      hour++;
+	  }
+	  if (hour >= 12) {
+	      hour = 0;
+	  }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -132,6 +193,36 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, LED_1_Pin|LED_2_Pin|LED_3_Pin|LED_4_Pin
+                          |LED_5_Pin|LED_6_Pin|LED_7_Pin|LED_8_Pin
+                          |LED_9_Pin|LED_10_Pin|LED_11_Pin|LED_12_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : LED_1_Pin LED_2_Pin LED_3_Pin LED_4_Pin
+                           LED_5_Pin LED_6_Pin LED_7_Pin LED_8_Pin
+                           LED_9_Pin LED_10_Pin LED_11_Pin LED_12_Pin */
+  GPIO_InitStruct.Pin = LED_1_Pin|LED_2_Pin|LED_3_Pin|LED_4_Pin
+                          |LED_5_Pin|LED_6_Pin|LED_7_Pin|LED_8_Pin
+                          |LED_9_Pin|LED_10_Pin|LED_11_Pin|LED_12_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
